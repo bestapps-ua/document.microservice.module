7b670a118c1906d5bd6f9e20f9467b2793e7be0c
Vitaliy Grinchishin <vitaliy.grinchishin@gmail.com>
2024-03-11 11:29:15 +0000

    v1.0.3

--- dist/model/v1/EntityModel.d.ts
+++ dist/model/v1/EntityModel.d.ts
@@ -1,16 +1,18 @@
 import APIEntityModel from "../api/v1/APIEntityModel";
 import IEntityModelOptions from "../../interface/IEntityModelOptions";
+import ApiEntity from "../../entity/api/v1/ApiEntity";
+import ApiList from "../../entity/api/v1/ApiList";
 declare class EntityModel {
     private _model;
     constructor(props: IEntityModelOptions);
     get model(): APIEntityModel;
     set model(value: APIEntityModel);
     action(action: string, params?: any): Promise<any>;
-    create(params?: any): Promise<any>;
-    get(params?: any): Promise<any>;
+    create(params?: any): Promise<ApiEntity>;
+    get(params?: any): Promise<ApiEntity>;
     delete(params?: any): Promise<boolean>;
-    set(params?: any): Promise<any>;
-    list(params?: any): Promise<any>;
+    set(params?: any): Promise<ApiEntity>;
+    list(params?: any): Promise<ApiList>;
 }
 export default EntityModel;
 //# sourceMappingURL=EntityModel.d.ts.map
--- src/model/v1/EntityModel.ts
+++ src/model/v1/EntityModel.ts
@@ -1,5 +1,7 @@
 import APIEntityModel from "../api/v1/APIEntityModel";
 import IEntityModelOptions from "../../interface/IEntityModelOptions";
+import ApiEntity from "../../entity/api/v1/ApiEntity";
+import ApiList from "../../entity/api/v1/ApiList";
 
 class EntityModel {
     private _model: APIEntityModel;
@@ -20,24 +22,24 @@ class EntityModel {
         return await this.model.action(action, params);
     }
 
-    async create(params: any = {}) {
+    async create(params: any = {}): Promise<ApiEntity> {
         return await this.action('create', params);
     }
 
-    async get(params: any = {}) {
+    async get(params: any = {}): Promise<ApiEntity> {
         return await this.action('get', params);
     }
 
-    async delete(params: any = {}) {
+    async delete(params: any = {}): Promise<boolean> {
         await this.action('delete', params);
         return true;
     }
 
-    async set(params: any = {}) {
+    async set(params: any = {}): Promise<ApiEntity> {
         return await this.action('set', params);
     }
 
-    async list(params: any = {}): Promise<any> {
+    async list(params: any = {}): Promise<ApiList> {
         return await this.action('list', params);
     }
 }
